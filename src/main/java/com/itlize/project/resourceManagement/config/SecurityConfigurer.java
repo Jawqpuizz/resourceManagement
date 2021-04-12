ec0906bb71852c37ca4c3819002e5c5be8bbf3bb
Jawqpuizz <45869766+Jawqpuizz@users.noreply.github.com>
2021-04-11 22:57:38 -0400

    Added security to the application

--- src/main/java/com/itlize/project/resourceManagement/config/SecurityConfigurer.java
+++ src/main/java/com/itlize/project/resourceManagement/config/SecurityConfigurer.java
@@ -36,7 +36,6 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     }
 
 
-
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
 //        http.csrf().disable();
